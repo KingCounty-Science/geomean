fb32d7465999723ff6513a294229fe87a47f0ac5
Daniel Nidzgorski <daniel.nidzgorski@kingcounty.gov>
2026-08-25 13:32:45 -0700

    Update documentation with method details.

--- R/geomean.R
+++ R/geomean.R
@@ -1,6 +1,7 @@
 #' Geomean
 #'
 #' Calculate the geometric mean of a vector of numbers.
+#' This uses the log-antilog method; it takes the natural logarithm of each number, averages them together, and then takes the antilog (i.e., exponent) of the result.
 #' @param x Vector of numeric inputs.
 #' @param na.rm Remove NAs from the data? Default is TRUE; otherwise any NAs will give a NA result.
 #' @return The geometric mean of x.
@@ -12,5 +13,7 @@
 
 geomean = function(x, na.rm=TRUE){
   exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+
+  cli::cli_alert_success("You have calculated the geometric mean of your numbers")
 }
 
